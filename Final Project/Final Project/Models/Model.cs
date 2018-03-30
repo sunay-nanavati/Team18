fdf9a2754a0bc437db546bee1da8b484c43ef180
sunay-nanavati <35670925+sunay-nanavati@users.noreply.github.com>
2018-03-30 12:38:44 -0500

    Update Model.cs

--- Final Project/Final Project/Models/Model.cs
+++ Final Project/Final Project/Models/Model.cs
@@ -28,32 +28,42 @@ namespace Final_Project.Models
     public class User
     {
         public Int32 UserID { get; set; }
-
+                
+        [Required(ErrorMessage = "Please enter valid Email")]
         public String Email { get; set; } 
-
+        
+        [Required(ErrorMessage = "Please enter a Password")]
         public String Password { get; set; }
-
+        
+        [Required(ErrorMessage = "Please enter a First Name")]
         [Display(Name = "First Name")]
         public String FirstName { get; set; }
 
+        [Required(ErrorMessage = "Please enter a Last Name")]
         [Display(Name = "Last Name")]
         public String LastName { get; set; }
-
+        
+        [Required(ErrorMessage = "Please enter Street Address")]
         [Display(Name = "Street Address")]
         public String StreetAddress { get; set; }
-
+        
+        [Required(ErrorMessage = "Please enter a valid City")]
         public String City { get; set; }
-
+        
+        [Required(ErrorMessage = "Please enter valid State")]
         public String State { get; set; }
 
+        [Required(ErrorMessage = "Please enter valid ZIP Code")]
         [Display(Name = "ZIP Code")]
         public Int32 ZipCode { get; set; }
 
+        [Required(ErrorMessage = "Please enter valid birthday")]
         public DateTime Birthday { get; set; }
 
         [Display(Name = "Popcorn Points")]
         public Int32 PopcornPoints { get; set; }
-
+        
+        [Required(ErrorMessage = "Please enter valid phone number")]
         [Display(Name = "Phone Number")]
         public Int32 PhoneNumber { get; set; }
 
