c67d698aa98ce84b87e46ab2496f7e8917f1bce1
sunay-nanavati <35670925+sunay-nanavati@users.noreply.github.com>
2018-04-04 16:39:20 -0500

    Update Review.cs

--- Final Project/Final Project/Models/Review.cs
+++ Final Project/Final Project/Models/Review.cs
@@ -8,20 +8,23 @@ namespace Final_Project.Models
 {
     public class Review
     {
+        [Required]
         [Display(Name = "Review ID")]
         public Int32 ReviewID { get; set; }
 
+        [Required]
         [Display(Name = "Review Score")]
         public Decimal ReviewScore { get; set; } //1.0 - 5.0, inclusive
 
+        [Required]
         [Display(Name = "Movie Review")]
         public String MovieReview { get; set; }
         
         [Display(Name = "Upvote")]
-        public Boolean Upvote { get; set; } //maybe enum here
+        public Boolean Upvote { get; set; } //maybe enum here; this might need to be Int32
         
         [Display(Name = "Approved")]
-        public Boolean Approved { get; set; }
+        public Boolean Approved { get; set; } //default to false (?)
 
         public Movie Movie { get; set; }
         public User User { get; set; }
