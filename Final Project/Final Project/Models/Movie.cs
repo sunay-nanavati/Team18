1e28d16daf7dd11698086e88f09951b43e6610f3
sunay-nanavati <35670925+sunay-nanavati@users.noreply.github.com>
2018-04-04 16:12:20 -0500

    Update Movie.cs

--- Final Project/Final Project/Models/Movie.cs
+++ Final Project/Final Project/Models/Movie.cs
@@ -8,22 +8,37 @@ namespace Final_Project.Models
 {
     public enum MPAA
     {
-        G, PG, PG13, R
+        G , PG , PG13 , R , NC17
     }
 
     public class Movie
     {
+        
         public Int32 MovieID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter valid Title")]
         public String Title { get; set; }
+        
         public String Tagline { get; set; }
 
+        [Required(ErrorMessage = "Please enter valid Release Year")]
         [Display(Name = "Release Year")]
         public DateTime ReleaseYear { get; set; }
+        
+        [Required(ErrorMessage = "Please enter valid running time")]
+        public Int32 RunTime { get; set; }
 
+        [Required(ErrorMessage = "Please enter valid Custoemr Rating")]
         [Display(Name = "Customer Rating")]
         public Decimal CustomerRating { get; set; } //must be between 1.0 and 5.0, inclusive
+        
+        [Required(ErrorMessage = "Please enter valid MPAA Rating")]
         public MPAA MPAA { get; set; }
+        
+        [Required(ErrorMessage = "Please enter at least one Actor")]
         public String Actors { get; set; }
+        
+        [Required(ErrorMessage = "Please enter Overview")]
         public String Overview { get; set; }
         
         public List<Showtime> Showtimes { get; set; }
