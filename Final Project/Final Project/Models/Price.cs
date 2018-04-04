b4721671a81902b3ec5474678a5061aa9c734831
sunay-nanavati <35670925+sunay-nanavati@users.noreply.github.com>
2018-04-04 16:34:42 -0500

    Price.cs

--- Final Project/Final Project/Models/Price.cs
+++ Final Project/Final Project/Models/Price.cs
@@ -8,12 +8,32 @@ namespace Final_Project.Models
 {
     public class Price
     {
+        [Required]
+        [Display(Name = "Price ID")]
         public Int32 PriceID { get; set; }
+        
+        [Required]
+        [Display(Name = "Matinee")]
         public Boolean IsMatinee { get; set; }
+        
+        [Required]
+        [Display(Name = "Teusday")]
         public Boolean IsTuesday { get; set; }
+        
+        [Required]
+        [Display(Name = "Senior")]
         public Boolean IsSenior { get; set; }
+        
+        [Required]
+        [Display(Name = "Late Weekday")]
         public Boolean IsLateWeekday { get; set; }
+        
+        [Required]
+        [Display(Name = "Weekend")]
         public Boolean IsWeekend { get; set; }
+        
+        [Required]
+        [Display(Name = "Two Days in Advance")]
         public Boolean IsTwoDaysAdvance { get; set; }
     }
 }
