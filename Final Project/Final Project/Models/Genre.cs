226a359fbfa935ee8a42b2b4cfe10dde8ae5b651
sunay-nanavati <35670925+sunay-nanavati@users.noreply.github.com>
2018-04-04 16:14:13 -0500

    Update Genre.cs

--- Final Project/Final Project/Models/Genre.cs
+++ Final Project/Final Project/Models/Genre.cs
@@ -9,6 +9,8 @@ namespace Final_Project.Models
     public class Genre
     {
         public Int32 GenreID { get; set; }
+        
+        [Required(ErrorMessage = "Please enter at least one Genre")]
         public String Name { get; set; }
 
         public List<Movie> Movies { get; set; }
