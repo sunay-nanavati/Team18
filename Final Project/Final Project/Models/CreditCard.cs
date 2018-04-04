c5dc35fcdb829d651a9ce3dd1558b634bd975f09
sunay-nanavati <35670925+sunay-nanavati@users.noreply.github.com>
2018-04-04 16:21:50 -0500

    Update CreditCard.cs

--- Final Project/Final Project/Models/CreditCard.cs
+++ Final Project/Final Project/Models/CreditCard.cs
@@ -13,29 +13,31 @@ namespace Final_Project.Models
     
     public class CreditCard
     {
+        [Required]
         public Int32 CreditCardID { get; set; }
 
-        [Display(Name = "Credit Card Type ")]
         [Required(ErrorMessage = "Please enter valid CreditCardType")]
+        [Display(Name = "Credit Card Type ")]
         public CreditCardType CreditCardType { get; set; }
-
-        [Display(Name = "Credit Card Number")]
+        
         [Required(ErrorMessage = "Please enter valid Credit Card Number")] //regex depends on cctype
+        [Display(Name = "Credit Card Number")]
         [DataType(DataType.CreditCard)]
         public Int32 CreditCardNumber { get; set; }
-
-        [Display(Name = "Expiration Date")]
+        
         [Required(ErrorMessage = "Please enter valid Expiration Date")]
+        [Display(Name = "Expiration Date")]   
         public DateTime ExpirationDate { get; set; }
 
-        [Display(Name = "CVV")]
         [Required(ErrorMessage = "Please enter valid CVV")] //regex depends on cctype
+        [Display(Name = "CVV")]  
         public Int32 CVV { get; set; }
-
+        
+        [Required]
         public String Name { get; set; }
 
-        [Display(Name = "Billing Address")]
         [Required(ErrorMessage = "Please enter valid Billing Address")]
+        [Display(Name = "Billing Address")]    
         public String BillingAddress { get; set; }
 
         public User User { get; set; }
